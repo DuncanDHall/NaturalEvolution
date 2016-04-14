1589d31a130eae20b96e40fff38aacce81566cd1
Duncan Hall <duncan@duncans-mbp.olin.edu>
2016-04-13 22:11:15 -0400

    cleaning things

--- nn_simple.py
+++ nn_simple.py
@@ -3,8 +3,8 @@ import math
 import random
 import time
 from pygame.locals import QUIT, KEYDOWN
-from random import choice
-from math import pi, sin, cos, atan
+# from random import choice
+# from math import pi, sin, cos, atan
 import numpy as np
 
 SCREEN_SIZE = (500, 500)
@@ -108,7 +108,6 @@ class Model(object):
 
         for i in range(0, BLOB_NUM):
             new_NN = NN(parents_NN=top_scoring)
-            # TODO: Check if dna results are the blobs or others >> hmm?
             self.blobs.append(Blob(self.foods[0], new_NN))
 
 
@@ -153,7 +152,6 @@ class NN(object):
 
     def process(self, z1):
         """ propigates the signal through the neural network """
-        #print z1
         # input and output to level 2 (nodes)
         z2 = z1.dot(self.W1)
         a2 = self.sigmoid(z2)
@@ -163,15 +161,13 @@ class NN(object):
         return a3
 
     def sigmoid(self, z):
-        # Apply sigmoid activation function (arctan):
-        # TODO: why does sigmoid not work?
-        #return z
+
         sig = 10*(1/(1+np.exp(-z))-.5)
-        return sig#50*(1/(1+np.exp(z)))
+        return sig
 
 
 class Blob(object):
-    """ Represents a ball in my brick breaker game """
+    """ Represents a blob agent """
     def __init__(self, target, nn=None):
         """ Create a ball object with the specified geometry """
         self.center_x = random.randint(0, SCREEN_SIZE[0])
@@ -189,7 +185,7 @@ class Blob(object):
         self.target = target
 
         # direction changes:
-        self.direction = random.uniform(0, 2*pi)
+        # self.direction = random.uniform(0, 2*pi)
 
         # Neural Network stuff here:
         if nn is not None:
@@ -202,15 +198,13 @@ class Blob(object):
             attributes
         """
         dist = abs(math.hypot(
-            self.center_x-other.center_x, self.center_y-other.center_y))
+            self.center_x - other.center_x, self.center_y - other.center_y))
         return dist < self.radius + other.radius
 
     def update(self, model):
         """ Update the position of the ball due to time passing """
         self.center_x += self.velocity_x
         self.center_y += self.velocity_y
-        # self.center_x += self.energy/50 * cos(self.direction)
-        # self.center_y += self.energy/50 * sin(self.direction)
 
         self.int_center = int(self.center_x), int(self.center_y)
 
@@ -233,7 +227,6 @@ class Blob(object):
                     self.energy = self.MAX_ENERGY
 
                 del model.foods[i]
-                # global SCREEN_SIZE
                 model.foods.append(
                     Food(
                         random.randint(10, SCREEN_SIZE[0]-10),
@@ -248,19 +241,12 @@ class Blob(object):
             self.center_y - self.target.center_y])
         acceleration_x, acceleration_y = tuple(self.nn.process(env))
 
-        # positions = np.array([
-        #     self.center_x, self.center_y,
-        # change matrix dimensions in model init and update:
-        #     # self.velocity_x, self.velocity_y,
-        #     self.target.center_x, self.target.center_y])
-        # acceleration_x, acceleration_y = tuple(self.DNA.dot(positions))
-
         self.velocity_x = acceleration_x
         self.velocity_y = acceleration_y
 
-        self.direction = atan(acceleration_x/acceleration_y)
+        # self.direction = atan(acceleration_x/acceleration_y)
 
-        self.direction = atan(acceleration_x/acceleration_y)
+        # self.direction = atan(acceleration_x/acceleration_y)
 
         if abs(self.velocity_x) > self.MAX_VELOCITY:
             self.velocity_x = (
@@ -273,18 +259,10 @@ class Blob(object):
 
     def score(self):
         return self.food_eaten
-        # final_dist_target = np.hypot(
-        #     self.center_x - self.target.center_x,
-        #     self.center_y - self.target.center_y
-        #     )
-        # return self.init_dist_target/final_dist_target
-        # return 1.0/(1 + np.hypot(
-        #     food.center_x-self.center_x,
-        #     food.center_y-self.center_y))
 
 
 class Food(object):
-    """ Represents a brick in my brick breaker game """
+    """ Represents a food entity """
     def __init__(self, center_x, center_y, radius):
         """ Initializes a Brick object with the specified
             geometry and color """
@@ -335,7 +313,6 @@ if __name__ == '__main__':
         if model.generation % SIM_SKIP_NUM == 0:
             view.draw()
             time.sleep(.001)
-
 
     # nn = NN()
     # z1 = np.array([-1, 1])
