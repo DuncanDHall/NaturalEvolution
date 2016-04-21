6122290667a0964583de77f9b24a6c69d396ed05
David Papp <david.papp@students.olin.edu>
2016-04-21 14:58:01 -0400

    updating food

---
+++ food.py
@@ -0,0 +1,11 @@
+class Food(object):
+    """ Represents a piece of food in our game. """
+    def __init__(self, center_x, center_y, radius):
+        """ Initializes a food object to a specified center and radius. """
+        self.center_x = center_x
+        self.center_y = center_y
+        self.radius = radius
+        #don't use color right now.  Maybe in the future change blobs to
+        #color of food that they are tracking
+        # self.color = color
+        self.eaten = False
