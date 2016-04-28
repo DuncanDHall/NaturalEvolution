4e64e502a8fbbef4c59b49c4fab16b299eea4ad1
Duncan Hall <duncan.hall@students.olin.edu>
2016-04-28 15:09:28 -0400

    actually fixed that

--- blob.py
+++ blob.py
@@ -36,7 +36,7 @@ class Blob(ParentSprite):
 
         # Neural Network stuff here:
         if nn is not None:
-            self.nn = NN(((1, nn),))
+            self.nn = nn
         else:
             self.nn = NN()
 
